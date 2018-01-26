731c0b6a9bdeabee319746fb88b88cde33910259
dongfang <dongfang@indeed.com>
2018-01-26 16:39:39 +0900

    remove logs

--- markComplete.js
+++ markComplete.js
@@ -10,19 +10,12 @@ function modifyDOM() {
         return;
     }
     const codeReviewIdx = document.URL.substr(codeReviewIdxStart);
-    console.log("code review idx: " + codeReviewIdx);
     const completeButtons = document.querySelectorAll('[id^="markCompleteButton"]');
-    console.log("complateButtons: " + completeButtons);
-    console.log("completeButtons.length: " + completeButtons.length);
     if (completeButtons.length > 0) {
         return;
     }
 
-    console.log('Tab script:');
-    console.log(document.body);
-
     const comments = document.querySelectorAll('[id^="inlinecommentContent"]');
-    console.log("comment length: " + comments.length);
     for (var i = 0; i < comments.length; ++i) {
         const commentActions = comments[i].getElementsByClassName('comment-actions-inner');
         var li = document.createElement("li");
@@ -66,25 +59,9 @@ function modifyDOM() {
     return document.body.innerHTML;
 }
 
-// document.getElementById("test").addEventListener('click', function() {
-//   console.log("Popup DOM fully loaded and parsed");
-//
-//   //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-//   chrome.tabs.executeScript({
-//     code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
-//   }, function(results){
-//     //Here we have just the innerHTML and not DOM structure
-//     console.log('Popup script:')
-//   console.log(results[0]);
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.executeScript({
         code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
     }, function (results) {
-        //Here we have just the innerHTML and not DOM structure
-        console.log('Popup script:');
-        console.log(results[0]);
     });
 });
